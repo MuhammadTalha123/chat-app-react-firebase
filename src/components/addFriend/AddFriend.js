6b6288764d11d0a6453698fce4a227e425be663d
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-05-02 02:43:58 +0500

    check user exist or not

--- src/components/addFriend/AddFriend.js
+++ src/components/addFriend/AddFriend.js
@@ -12,14 +12,15 @@
 
 // export default AddFriend;
 
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { app } from "../../firebase/firebaseConfig";
 
 function FormDialog() {
   const [open, setOpen] = React.useState(false);
@@ -32,21 +33,43 @@ function FormDialog() {
     setOpen(false);
   };
 
+  const handleInviteFriend = () => {
+    let inputValue = document.getElementById("invite-email-input").value;
+    console.log(inputValue);
+    const usersRef = app.firestore().collection("users").doc(inputValue);
+
+    usersRef.get().then((docSnapshot) => {
+      if (docSnapshot.exists) {
+        usersRef.onSnapshot((doc) => {
+          // do stuff with the data
+          console.log("user exist.");
+        });
+      } else {
+        console.log("user not exist.");
+        // create the document
+      }
+    });
+  };
+
   return (
     <div>
       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-      <i className="fad fa-plus" style={{fontSize:"50px"}}></i>
+        <i className="fas fa-plus" style={{ fontSize: "50px" }}></i>
       </Button>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
         <DialogTitle id="form-dialog-title">Invite Friends</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            Invite your freinds to connect with chat video and photos.
+            Invite your freinds to connect with chat video's and photo's.
           </DialogContentText>
           <TextField
             autoFocus
             margin="dense"
-            id="name"
+            id="invite-email-input"
             label="Email Address"
             type="email"
             fullWidth
@@ -56,8 +79,8 @@ function FormDialog() {
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={handleClose} color="primary">
-            Send Email
+          <Button onClick={handleInviteFriend} color="primary">
+            Invite Friend
           </Button>
         </DialogActions>
       </Dialog>
