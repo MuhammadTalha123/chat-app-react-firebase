497780dd497c487c46d43cfc3905e40822be0d03
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-05-06 03:04:59 +0500

    adding request page

--- src/pages/friendRequest/FriendRequest.js
+++ src/pages/friendRequest/FriendRequest.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import history from "../../routes/history";
+import { app } from "../../firebase/firebaseConfig";
 
 const FriendRequest = () => {
+  const [requests, setRequests] = useState([]);
+  let myEmail = localStorage.getItem("email");
+  const myRef = app.firestore().collection("users").doc(myEmail);
+  myRef.onSnapshot((docSnapshot) => {
+    setRequests(docSnapshot.data().friendsRequest);
+  });
   return (
     <div>
       <div>
         <button onClick={() => history.push("/")}>Go Home</button>
       </div>
-      <div className="friend_request_container">
-        <div className="friend_request_name">
-          <h2>Talha</h2>
-        </div>
-        <div className="friend_request_button">
-          <button className="friend_request_accept">ACCEPT</button>
-          <button className="friend_request_decline">DECLINE</button>
-        </div>
-        <hr />
-      </div>
+      {requests.map((item) => {
+        return (
+          <div className="friend_request_container">
+            <div className="friend_request_name">
+              <h2>{item.from}</h2>
+            </div>
+            <div className="friend_request_button">
+              <button className="friend_request_accept">ACCEPT</button>
+              <button className="friend_request_decline">DECLINE</button>
+            </div>
+            <hr />
+          </div>
+        );
+      })}
     </div>
   );
 };
