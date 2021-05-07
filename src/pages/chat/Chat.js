917a4676e40829406173583a0f74dec2a1d877e6
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-05-08 02:17:56 +0500

    adding chat ui

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -8,6 +8,13 @@ const Chat = () => {
             <Navbar />
             <button onClick={()=>history.push("/")}>Go Home</button>
             <h1>Chat</h1>
+            <div className="chat_paragraph">
+                <p className="user_text" style={{textAlign: "right"}}>User</p>
+                <p className="friend_text" style={{textAlign: "left"}}>Friends</p>
+            </div>
+            <div className="msg_input_div" style={{textAlign: "center"}}>
+                <input type="text" id="msg_input" placeholder="Message Text..." />
+            </div>
         </div>
     );
 };
