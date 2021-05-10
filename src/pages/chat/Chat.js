4b1f48b2250e986b28c0c61577e7d61b2f2ae62e
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-05-11 01:48:01 +0500

    sending and revieving messages

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import history from "../../routes/history";
 import { app } from "../../firebase/firebaseConfig";
 
 const Chat = () => {
+  const [sentMsg, setSentMsg] = useState([]);
+  const [recievedMsg, setRecievedMsg] = useState([]);
+  useEffect(() => {
+    let myEmail = localStorage.getItem("email");
+    let userEmail = window.location.href.slice(
+      window.location.href.lastIndexOf("/") + 1
+    );
+    app
+      .firestore()
+      .collection("chat")
+      .where("from", "==", myEmail)
+      .where("to", "==", userEmail + ".com")
+      .onSnapshot((resp) => {
+        // console.log(resp.docs[0].data());
+        setSentMsg(resp.docs);
+      });
+
+    app
+      .firestore()
+      .collection("chat")
+      .where("to", "==", myEmail)
+      .where("from", "==", userEmail + ".com")
+      .onSnapshot((resp) => {
+        // console.log(resp.docs[0].data());
+        setRecievedMsg(resp.docs);
+      });
+  }, []);
   const handleSendMsg = () => {
     let msgInputValue = document.getElementById("msg_text").value;
-    console.log(msgInputValue);
     let myEmail = localStorage.getItem("email");
-    let userEmail = window.location.href.slice(window.location.href.lastIndexOf("/")+1);
-    console.log(userEmail, myEmail);
-
+    let userEmail = window.location.href.slice(
+      window.location.href.lastIndexOf("/") + 1
+    );
     app
       .firestore()
       .collection("chat")
@@ -33,18 +59,26 @@ const Chat = () => {
       <button onClick={() => history.push("/")}>Go Home</button>
       <h1>Chat</h1>
       <div className="chat_paragraph">
-        <p
-          className="user_text"
-          style={{ textAlign: "right", marginRight: "20px" }}
-        >
-          User
-        </p>
-        <p
-          className="friend_text"
-          style={{ textAlign: "left", marginLeft: "20px" }}
-        >
-          Friends
-        </p>
+        {sentMsg.map((item) => {
+          return (
+            <p
+              className="user_text"
+              style={{ textAlign: "right", marginRight: "20px" }}
+            >
+              {item.data().message}
+            </p>
+          );
+        })}
+        {recievedMsg.map((item) => {
+          return (
+            <p
+              className="friend_text"
+              style={{ textAlign: "left", marginLeft: "20px" }}
+            >
+              {item.data().message}
+            </p>
+          );
+        })}
       </div>
       <div className="msg_input_div" style={{ textAlign: "center" }}>
         <input type="text" id="msg_text" placeholder="Message Text..." />
